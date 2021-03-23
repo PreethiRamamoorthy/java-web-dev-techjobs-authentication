329a72d1bf71fbd7b8028b548fcd187bab33b472
Preethi <preethi.ramamoorthy@gmail.com>
2021-03-22 21:02:46 -0500

    Final Commit- Studio 19 - TechJobs Auth

--- src/main/java/org/launchcode/javawebdevtechjobsauthentication/AuthenticationFilter.java
+++ src/main/java/org/launchcode/javawebdevtechjobsauthentication/AuthenticationFilter.java
@@ -5,7 +5,6 @@ import org.launchcode.javawebdevtechjobsauthentication.models.User;
 import org.launchcode.javawebdevtechjobsauthentication.models.data.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -15,11 +14,29 @@ import java.util.List;
 
 public class AuthenticationFilter extends HandlerInterceptorAdapter {
 
-    @Autowired
-    UserRepository userRepository;
+   @Autowired
+   UserRepository userRepository;
 
     @Autowired
-    AuthenticationController authenticationController;
+   AuthenticationController authenticationController;
+
+//    @Override
+//    public boolean preHandle(HttpServletRequest request,
+//                             HttpServletResponse response,
+//                             Object handler) throws IOException {
+//
+//        HttpSession session = request.getSession();
+//        User user = authenticationController.getUserFromSession(session);
+//
+//        // The user is logged in
+//        if (user != null) {
+//            return true;
+//        }
+//
+//        // The user is NOT logged in
+//        response.sendRedirect("/login");
+//        return false;
+//    }
 
     private static final List<String> whitelist = Arrays.asList("/login", "/register", "/logout", "/css");
 
--- src/main/java/org/launchcode/javawebdevtechjobsauthentication/WebApplicationConfig.java
+++ src/main/java/org/launchcode/javawebdevtechjobsauthentication/WebApplicationConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.launchcode.javawebdevtechjobsauthentication.controllers.AuthenticationController;
+
 
 @Configuration
 public class WebApplicationConfig implements WebMvcConfigurer {
